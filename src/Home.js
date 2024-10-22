9dcd94fdcf845c00a303cf3cc9572e9030e5d5de
KTsybulka <ktsybulka11@gmail.com>
2024-10-22 10:39:47 -0300

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
 
     const [blogs, setBlog] = useState(null);
     const [isPending, setIsPanding] = useState(true);
+    const [error, setError] = useState(null);
 
     // useEffect(() =>{
     //     fetch('http://localhost:8000/blogs')
@@ -22,18 +23,30 @@ const Home = () => {
         setTimeout(() => {
                  fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok){
+                throw Error('could not fetch the data for that resourse')
+            }
+            // console.log(res);
             return res.json();
         })
         .then((data) => {
             // console.log(data);
             setBlog(data);
             setIsPanding(false);
-        });
-    }, 1000)   
+            setError(null);
+        })
+        .catch(err => {
+            // console.log(err.message);
+            setIsPanding(false);
+            setError(err.message);
+        })
+    }, 500)   
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+
             {isPending && <div>Loading ...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
             
