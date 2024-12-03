1182d0d0b6a6c822c483c1c38afb088619a9b9da
KTsybulka <ktsybulka11@gmail.com>
2024-12-03 12:16:32 -0500

    Delete blogs

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../useFetch";
+import { fireEvent } from "@testing-library/react";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log('blog deleted');
+            navigate('/');
+        }); // Fixed missing closing parenthesis here
+    };
 
 return (
     <div className="blog-details">
@@ -15,6 +27,7 @@ return (
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author}</p>
                 <div>{blog.body} </div>
+                <button onClick={handleClick}>Delete</button>
             </article> 
             )}   
         </div>
